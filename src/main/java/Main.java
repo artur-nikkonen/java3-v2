2826a972d365dd121d069d7ad0f1600a020d234d
Artur Nikkonen <artur.nikkonen@gmail.com>
2020-07-21 21:52:33 +0300

    Вопрос про компаратор

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        Stream<Integer> integerStream = Stream.of(1, 2, 3, 3, 7);
+        Test1(integerStream);
+    }
+
+    public static List<Integer> Test1(Stream<Integer> stream) {
+
+        Comparator<Integer> comparator1 = Comparator.comparing(x -> x);
+        comparator1 = comparator1.reversed();
+        comparator1 = comparator1.thenComparing(x->x);
+
+        return stream.sorted(comparator1).collect(Collectors.toList());
+    }
+    public static List<Integer> Test2(Stream<Integer> stream) {
+        //не понимаю, как скомбинировать компаратор
+        //компилятьр ругается на ттипы, но я не понимаю, как это исправить
+        Comparator<Integer> comparator1 = Comparator.comparing(x -> x).reversed().thenComparing(x->x);
+
+        return stream.sorted(comparator1).collect(Collectors.toList());
+    }
+}
