599f43b9f7ba6d7445b826eabd4c19714136e447
Artur Nikkonen <artur.nikkonen@gmail.com>
2020-07-22 14:51:54 +0300

    Sorted problem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,47 @@
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
 public class Main {
+
     public static void main(String[] args) {
-        Stream<Integer> integerStream = Stream.of(1, 2, 3, 3, 7);
-        Test1(integerStream);
-    }
+        Person[] people = new Person[]{
+                new Person("Ivan", 10),
+                new Person("Petr", 8),
+                new Person("Sasha", 12),
+        };
 
-    public static List<Integer> Test1(Stream<Integer> stream) {
+        Stream<Person> stream = Arrays.stream(people);
 
-        Comparator<Integer> comparator1 = Comparator.comparing(x -> x);
-        comparator1 = comparator1.reversed();
-        comparator1 = comparator1.thenComparing(x -> x);
+        //РЕШЕНИЕ: https://stackoverflow.com/a/24437207
 
-        return stream.sorted(comparator1).collect(Collectors.toList());
-    }
-
-    public static List<Integer> Test2(Stream<Integer> stream) {
-        //не понимаю, как скомбинировать компаратор
-        //компилятор ругается на типы, но я не понимаю, как это исправить
-        Comparator<Integer> comparator1 = Comparator.comparing(x -> x).reversed().thenComparing(x -> x);
-
-        return stream.sorted(comparator1).collect(Collectors.toList());
-    }
-
-    public static List<Integer> Test2(Stream<Integer> stream) {
-        //не понимаю, как скомбинировать компаратор
-        //компилятор ругается на типы, но я не понимаю, как это исправить
-        Comparator<Integer> comparator1 = Comparator.<Integer, Integer>comparing(x -> x).reversed().thenComparing(x -> x);
-
+        //Работает
+        Stream<Person> sorted1 = stream.sorted(Comparator.comparing(x -> x.getName()));
+        //Работает!
+        Stream<Person> sorted2 = stream.sorted(Comparator.comparing(Person::getName).reversed().thenComparing(Person::getAge));
+        //Работает
+        Stream<Person> sorted4 = stream.sorted(Comparator.comparing((Person x) -> x.getName()).reversed().thenComparing(Person::getAge));
+        //Снова работает!
+        Stream<Person> sorted5 = stream.sorted(Comparator.comparing(Person::getName).reversed().thenComparing(x -> x.getAge()));
+        //Не работает
+        Stream<Person> sorted3 = stream.sorted(Comparator.comparing(x -> x.getName()).reversed().thenComparing(Person::getAge));
     }
 }
